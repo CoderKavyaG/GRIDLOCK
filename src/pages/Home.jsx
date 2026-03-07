d3a5c0e9daada4c595c4cc95d15471c08b3324d5
CoderKavyaG <codecraftkavya@gmail.com>
2026-03-07 21:18:28 +0530

    init: project setup with Vite, Tailwind, Framer Motion, RAWG API config

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="min-h-screen">
+            {/* Home page components will go here */}
+        </div>
+    );
+};
+
+export default Home;
